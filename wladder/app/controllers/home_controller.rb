5af8123c9b33ff2bd154d3a95f2ee1c9be8bc3ad
Philip Strachan <pstrachan@mail.csuchico.edu>
2016-06-15 06:50:56 +0000

    Now sends beginning and end word to show page.

--- wladder/app/controllers/home_controller.rb
+++ wladder/app/controllers/home_controller.rb
@@ -7,10 +7,12 @@ class HomeController < ApplicationController
 
   def show
     @words=Array.new
-    @words[0]=params[:word1] if params[:word1]!=""
-    @words[1]=params[:word2] if params[:word2]!=""
-    @words[2]=params[:word3] if params[:word3]!=""
-    @words[3]=params[:word4] if params[:word4]!=""
-    @words[4]=params[:word5] if params[:word5]!=""
+    @words[0]=params[:word0]
+    @words[1]=params[:word1] if params[:word1]!=""
+    @words[2]=params[:word2] if params[:word2]!=""
+    @words[3]=params[:word3] if params[:word3]!=""
+    @words[4]=params[:word4] if params[:word4]!=""
+    @words[5]=params[:word5] if params[:word5]!=""
+    @goal=params[:goal]
   end
 end
