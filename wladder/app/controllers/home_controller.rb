4e2b673401b134fc8fe781e8f576e3214200cb71
Philip Strachan <pstrachan@mail.csuchico.edu>
2016-06-15 06:23:29 +0000

    Show page now displays correct amount of guesses.

--- wladder/app/controllers/home_controller.rb
+++ wladder/app/controllers/home_controller.rb
@@ -5,10 +5,10 @@ class HomeController < ApplicationController
 
   def show
     @words=Array.new
-    @words[0]=params[:word1]
-    @words[1]=params[:word2]
-    @words[2]=params[:word3]
-    @words[3]=params[:word4]
-    @words[4]=params[:word5]
+    @words[0]=params[:word1] if params[:word1]!=""
+    @words[1]=params[:word2] if params[:word2]!=""
+    @words[2]=params[:word3] if params[:word3]!=""
+    @words[3]=params[:word4] if params[:word4]!=""
+    @words[4]=params[:word5] if params[:word5]!=""
   end
 end
