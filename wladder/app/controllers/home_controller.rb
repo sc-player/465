ef79fff1ae1625385b07a5ed5ba630625ae3fb35
Philip Strachan <pstrachan@mail.csuchico.edu>
2016-06-15 07:08:58 +0000

    Completed validation of word using functions in ladder.rb. Changed controller to only create an array large enough to hold the guesses that aren't empty. Show page displays victory or failure message correctly.

--- wladder/app/controllers/home_controller.rb
+++ wladder/app/controllers/home_controller.rb
@@ -7,12 +7,13 @@ class HomeController < ApplicationController
 
   def show
     @words=Array.new
-    @words[0]=params[:word0]
-    @words[1]=params[:word1] if params[:word1]!=""
-    @words[2]=params[:word2] if params[:word2]!=""
-    @words[3]=params[:word3] if params[:word3]!=""
-    @words[4]=params[:word4] if params[:word4]!=""
-    @words[5]=params[:word5] if params[:word5]!=""
-    @goal=params[:goal]
+    @words << params[:word0]
+    @words << params[:word1] if params[:word1]!=""
+    @words << params[:word2] if params[:word2]!=""
+    @words << params[:word3] if params[:word3]!=""
+    @words << params[:word4] if params[:word4]!=""
+    @words << params[:word5] if params[:word5]!=""
+    @words << params[:goal]
+    @victory=legal @words
   end
 end
