345890b33c54f51f8b7b5b0e0925b23639956c09
Philip Strachan <pstrachan@mail.csuchico.edu>
2016-06-15 06:00:08 +0000

    Basic page structure complete, all 5 guess are passed through the form

--- wladder/app/controllers/home_controller.rb
+++ wladder/app/controllers/home_controller.rb
@@ -4,10 +4,11 @@ class HomeController < ApplicationController
   end
 
   def show
-    @words[0]=params[:words]
-    @words[1]=params[:words]
-    @words[2]=params[:words]
-    @words[3]=params[:words]
-    @words[4]=params[:words]
+    @words=Array.new
+    @words[0]=params[:word1]
+    @words[1]=params[:word2]
+    @words[2]=params[:word3]
+    @words[3]=params[:word4]
+    @words[4]=params[:word5]
   end
 end
