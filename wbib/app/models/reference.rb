edb28bf95674341bc0b39cb1f74bf97cf4f6c92f
Philip Strachan <pstrachan@mail.csuchico.edu>
2016-06-19 19:13:51 +0000

    Added validation. Still untested.

--- wbib/app/models/reference.rb
+++ wbib/app/models/reference.rb
@@ -1,2 +1,12 @@
 class Reference < ActiveRecord::Base
+  before_validation :validURL
+  validates :URL, presence: true
+  validates :topic, presence: true
+  validates :annotation, presence:true
+  protected
+    def validURL
+      self.URL.prepend "http:\\" if self.URL =~ /^http[s]{1}:\/\//
+    end
 end
+
+
