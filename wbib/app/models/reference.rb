5f22d57747ca2caf8e72a7ea8661b59cca841d0c
Philip Strachan <pstrachan@mail.csuchico.edu>
2016-06-19 20:34:39 +0000

    Fixed adding http:// to a web address.

--- wbib/app/models/reference.rb
+++ wbib/app/models/reference.rb
@@ -5,7 +5,7 @@ class Reference < ActiveRecord::Base
   validates :annotation, presence:true
   protected
     def validURL
-      self.URL.prepend "http:\\" if self.URL =~ /^http[s]{1}:\/\//
+      self.URL.prepend "http://" unless self.URL =~ /^http[s]{1}:\/\//
     end
 end
 
