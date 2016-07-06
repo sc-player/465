f2b3b090dc4c179619701f9a1f3e979de2ff6e32
Philip Strachan <pstrachan@mail.csuchico.edu>
2016-07-06 04:02:38 +0000

    Progress bar added, task show page is looking up!

--- tasks/app/models/task.rb
+++ tasks/app/models/task.rb
@@ -2,4 +2,7 @@ class Task < ActiveRecord::Base
   belongs_to :user
   has_many :subtasks, dependent: :destroy
   accepts_nested_attributes_for :subtasks
+  def get_total_percent
+     subtasks.map(&:percent).sum.to_s
+  end
 end
