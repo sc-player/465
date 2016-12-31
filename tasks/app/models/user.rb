4be65dfa1a02ba57e3ccaf9b8ccbe1dbb9ad2831
Philip Strachan <pstrachan@mail.csuchico.edu>
2016-12-31 00:18:16 +0000

    Added validation for username uniqueness.
    To Do:
      Move back button to top
      Add fixed header to task view page
      RESTful split interface
      User interconnectivity

--- tasks/app/models/user.rb
+++ tasks/app/models/user.rb
@@ -2,8 +2,13 @@ class User < ActiveRecord::Base
 #   Include default devise modules. Others available are:
 #   :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
   has_many :tasks, dependent: :destroy
+  validates_presence_of :username
+  validates_uniqueness_of :username
+  validates_presence_of :password, on: :create
+  validates_confirmation_of :password, on: :create
+  validates_length_of :password, within: Devise.password_length, allow_blank: true
 
   def email_required?
     false
